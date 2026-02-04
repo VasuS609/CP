e8551e5098209d871d6b3b6815f7aa32af2a5f94
VasuS609 <vasusahu4008@gmail.com>
2026-02-05 04:35:29 +0530

    binary search tree

---
+++ DSA/BST/Basics/Whileloop.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int n = nums.size();
+        int high = n-1;
+
+        while(low<=high){
+            int mid = (int)(low + high) / 2;
+            if(target == nums[mid]) return mid;
+            else if( target < nums[mid]) high = mid-1;
+            else low = mid+1;
+        }
+        return -1;
+    }
+};
